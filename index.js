c30992a0040a68706f3f555a4e6ff03c6222b065
arnobtarafder <rmtarnob@gmail.com>
2022-04-30 11:49:35 +0600

    get connection to database & add primary setup

---
+++ index.js
@@ -0,0 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k40me.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        await client.connect();
+        // const furnitureCollection = client.db("AzimWare").collection("furniture");
+        
+        // app.get("/furniture")
+        console.log("database connected");
+    }
+    
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+
+app.get("/", (req, res) => {
+    res.send("I am the home page of Azimuth Warehouse");
+})
+
+app.listen(port, () => {
+    console.log("Listening to the port", port + 2000);
+})
