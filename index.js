2679b45b054ee98f0a53681973a89665e1f04183
arnobtarafder <rmtarnob@gmail.com>
2022-05-04 01:55:44 +0600

    add: JSON Web Token functionalities

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ app.use(cors());
 app.use(express.json());
 
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized access" })
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" })
+        }
+        // console.log("decoded", decoded);
+        req.decoded = decoded;
+        next();
+    })
+    // console.log("inside verifyJWT", authHeader);
+}
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.k40me.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -19,6 +37,17 @@ async function run() {
         await client.connect();
         console.log("database connected");
         const furnitureCollection = client.db("AzimWare").collection("furniture");
+
+
+           // AUTH
+           app.post("/user", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: "1d"
+            });
+            res.send({ accessToken })
+        })
+
 
         // GET ITEMS  
         app.get("/products", async (req, res) => {
@@ -38,6 +67,7 @@ async function run() {
         })
 
 
+
         // QUANTITY UPDATE BY PUT
         app.put("/products/:id", async (req, res) => {
             const id = req.params.id;
@@ -55,6 +85,7 @@ async function run() {
             res.send(result);
         });
 
+
         // ADD ITEMS
         app.post("/products", async (req, res) => {
             const newProduct = req.body;
@@ -62,6 +93,23 @@ async function run() {
             res.send(result)
         })
 
+
+        
+
+        app.get('/myItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' })
+            }
+        })
+        
 
         // DELETE ITEM 
         app.delete("/products/:id", async(req, res) => {
