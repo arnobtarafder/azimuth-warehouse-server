447bd6391337cef129b9cf78a92a21e7dcdcae2b
arnobtarafder <rmtarnob@gmail.com>
2022-05-04 02:24:13 +0600

    JWT protected by env file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require("dotenv").config();
-
 const app = express();
 const port = process.env.PORT || 5000;
 
